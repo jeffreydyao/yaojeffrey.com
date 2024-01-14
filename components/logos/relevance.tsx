d9809bf2cec94e5688be2093bdc315e1e4853b3f
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2024-01-14 14:43:18 +1100

    feat: company logos

---
+++ components/logos/relevance.tsx
@@ -0,0 +1,51 @@
+export function Relevance({ className }: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 25 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16.5711 16.4999C21.1275 16.4999 24.8211 12.8063 24.8211 8.24996C24.8211 3.69363 21.1275 0 16.5711 0C12.0148 0 8.32117 3.69363 8.32117 8.24996C8.32117 12.8063 12.0148 16.4999 16.5711 16.4999Z"
+        fill="url(#paint0_linear_1_24)"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M6.31067 7.30453H5.34561C2.39331 7.30453 0 9.69781 0 12.6502V18.6544C0 21.6067 2.39332 24 5.34561 24H11.3498C14.2593 24 16.6259 21.6755 16.6939 18.7824C10.931 18.7824 6.25917 14.1107 6.25917 8.34781C6.25917 7.9957 6.27661 7.64768 6.31067 7.30453ZM8.41066 7.30453C8.36805 7.64631 8.3461 7.9945 8.3461 8.34781C8.3461 12.9581 12.0835 16.6955 16.6938 16.6955H16.6954V12.6502C16.6954 9.69781 14.3021 7.30453 11.3498 7.30453H8.41066Z"
+        fill="url(#paint1_linear_1_24)"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M16.6954 16.5C12.0857 16.4992 8.34904 12.9573 8.34904 8.34746C8.34904 7.99427 8.37098 7.6462 8.41357 7.30453H11.3498C14.3021 7.30453 16.6954 9.69781 16.6954 12.6502V16.5Z"
+        fill="#B5B5B5"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1_24"
+          x1="12.0281"
+          y1="13.3314"
+          x2="24.9466"
+          y2="6.64939"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#D9D9D9" />
+          <stop offset="1" stop-color="#E3E3E3" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_1_24"
+          x1="11.5822"
+          y1="7.09485"
+          x2="8.34772"
+          y2="24"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#8F8F8F" />
+          <stop offset="1" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
